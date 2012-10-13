2986eb8b702266d518652a10524e9296ff3e5704
Martin Helmich <typo3@martin-helmich.de>
2012-10-13 17:21:21 +0200

    [TASK] Kickstarted entry repository.

---
+++ Classes/MH/Guestbook/Domain/Repository/EntryRepository.php
@@ -0,0 +1,21 @@
+<?php
+namespace MH\Guestbook\Domain\Repository;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "MH.Guestbook".          *
+ *                                                                        *
+ *                                                                        */
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * A repository for Entries
+ *
+ * @Flow\Scope("singleton")
+ */
+class EntryRepository extends \TYPO3\Flow\Persistence\Repository {
+
+	// add customized methods here
+
+}
+?>
